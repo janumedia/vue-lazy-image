e8293c3e63ac0120fc559f73040638ad526c79cc
janumedia@yahoo.com.sg <janumedia@gmail.com>
2019-01-27 17:10:15 +0800

    add unit test

---
+++ tests/unit/LazyImage.spec.js
@@ -0,0 +1,33 @@
+import { shallowMount } from '@vue/test-utils'
+import LazyImage from '@/LazyImage'
+
+const createWrapper = propsData => shallowMount(LazyImage, {
+    attachToDocument: true,
+    propsData
+});
+
+describe('LazyImage.vue', () => {
+    let wrapper,
+        IMAGE               = 'image.jpg',
+        IMAGE_PLACEHOLDER   = 'custom-image-placeholder.jpg',
+        props               = { src: IMAGE, alt: 'image title'};
+    
+    beforeAll(() => {
+        LazyImage.components = LazyImage.components || {};
+    });
+
+    afterEach(() => {
+        //destory wrapper
+        wrapper.destroy();
+    });
+
+    it('should render LazyImage default', () => {
+        wrapper = createWrapper(props);
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+
+    it('should render LazyImage custom placeHolder', () => {
+        wrapper = createWrapper({...props, placeHolder: IMAGE_PLACEHOLDER});
+        expect(wrapper.html()).toMatchSnapshot();
+    });
+})
